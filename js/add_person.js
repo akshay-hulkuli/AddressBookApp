4ef412585b9ae0f6d2731148d9ca4e5055b62999
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-19 18:56:46 +0530

    ADDED: method to populate contact data json object

--- js/add_person.js
+++ js/add_person.js
@@ -1,18 +1,20 @@
+let contactDataObj = {};
+
 window.addEventListener('DOMContentLoaded', () =>{
     const name = document.querySelector('#name');
     const textError = document.querySelector('.text-error');
     name.addEventListener('input', function() {
-      if(name.value.length == 0){
-        textError.textContent = "";
-        return;
-      }
-      try{
+        if(name.value.length == 0){
+            textError.textContent = "";
+            return;
+        }
+        try{
         checkName(name.value);
-        textError.textContent = "";
-      } 
-      catch(e) {
-        textError.textContent = e;
-      }
+            textError.textContent = "";
+        } 
+        catch(e) {
+            textError.textContent = e;
+        }
     });
 
     const phone = document.querySelector('#phone');
@@ -50,16 +52,41 @@ window.addEventListener('DOMContentLoaded', () =>{
     const zip = document.querySelector('#zip');
     const zipError = document.querySelector('.zip-error');
     zip.addEventListener('input', function() {
-      if(zip.value.length == 0){
-        zipError.textContent = "";
-        return;
-      }
-      try{
+        if(zip.value.length == 0){
+            zipError.textContent = "";
+            return;
+        }
+        try{
         checkZip(zip.value);
-        zipError.textContent = "";
-      } 
-      catch(e) {
-        zipError.textContent = e;
-      }
+            zipError.textContent = "";
+        } 
+        catch(e) {
+            zipError.textContent = e;
+        }
     });
-  });
+});
+
+const save = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    setContactDataObj();
+    alert(JSON.stringify(contactDataObj));
+}
+
+const setContactDataObj = () => {
+    contactDataObj.id = createContactId();
+    contactDataObj._name = getValueById('#name');
+    contactDataObj._phoneNumber = getValueById('#phone');
+    contactDataObj._address = getValueById('#address');
+    contactDataObj._city = getValueById('#city');
+    contactDataObj._state = getValueById('#state');
+    contactDataObj._zip = getValueById('#zip');
+}
+
+const createContactId = () => {
+    return new Date().getTime();
+}
+
+const getValueById = (value) => {
+    return document.querySelector(value).value;
+}
