dad936a3c95100e28dc5e066f7d11e0e8737a508
Akshaya HP <akshayhulkuli@gmail.com>
2021-10-20 20:27:28 +0530

    REFACTORED: exporting site_properties using ES6 export statement

--- js/site_properties.js
+++ js/site_properties.js
@@ -2,5 +2,7 @@ let site_properties = {
     home_page: "../pages/address_book_home.html",
     add_person_page: "../pages/add_person.html",
     server_url: "http://localhost:3000/addressBookDB/",
-    use_local_storage: "true"
+    use_local_storage: "false"
 };
+
+export {site_properties};
